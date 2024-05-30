44813768d2ecad15775b771625c07fbf661218ad
AlvigoBuana <www.alvigowahyubuana@gmail.com>
2024-05-30 17:04:00 +0700

    praktikum8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img alt="card_changli" class="changli" src="@/assets/card_changli.webp" width="300" height="400" />
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+    </div>
+</template>
